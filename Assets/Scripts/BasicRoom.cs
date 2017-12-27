95c6e642d73841b35502eb46dd2f0bc93578d5a6
Michael Frederick <master16789@gmail.com>
2017-12-27 09:26:43 -0500

    Create BasicRoom and RoomGeneration scene
    Scene to get started and create basic room

---
+++ Assets/Scripts/BasicRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRoom : MonoBehaviour {
+
+    public GameObject TopDoor;
+    public GameObject BottomDoor;
+    public GameObject LeftDoor;
+    public GameObject RightDoor;
+
+    public int Height;
+    public int Width;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}

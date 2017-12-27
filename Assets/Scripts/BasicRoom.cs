200c772852dd8be2d5e288051a1fe4cb08aedc7c
Michael Frederick <master16789@gmail.com>
2017-12-27 11:23:26 -0500

    Generating rooms to the right

--- Assets/Scripts/BasicRoom.cs
+++ Assets/Scripts/BasicRoom.cs
@@ -9,16 +9,90 @@ public class BasicRoom : MonoBehaviour {
     public GameObject LeftDoor;
     public GameObject RightDoor;
 
-    public int Height;
-    public int Width;
+    [HideInInspector]
+    public float Height;
+    [HideInInspector]
+    public float Width;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        Height = TopDoor.transform.position.y * 2;
+        Width = RightDoor.transform.position.x * 2;
 	}
-	
-	//// Update is called once per frame
-	//void Update () {
-		
-	//}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    public void CloseDoor(Direction doorToClose)
+    {
+        Debug.LogFormat("Closing Door {0}", doorToClose);
+        // Close the door by activating the door object
+        switch(doorToClose)
+        {
+            case Direction.Top:
+                TopDoor.SetActive(true);
+                break;
+            case Direction.Bottom:
+                BottomDoor.SetActive(true);
+                break;
+            case Direction.Left:
+                LeftDoor.SetActive(true);
+                break;
+            case Direction.Right:
+            default:
+                RightDoor.SetActive(true);
+                break;
+        }
+
+        return;
+    }
+
+    public void OpenDoor(Direction doorToOpen)
+    {
+        Debug.LogFormat("Opening Door {0}", doorToOpen);
+        // Open the door by deactivating the door object
+        switch (doorToOpen)
+        {
+            case Direction.Top:
+                TopDoor.SetActive(false);
+                break;
+            case Direction.Bottom:
+                BottomDoor.SetActive(false);
+                break;
+            case Direction.Left:
+                LeftDoor.SetActive(false);
+                break;
+            case Direction.Right:
+            default:
+                RightDoor.SetActive(false);
+                break;
+        }
+
+        return;
+    }
+
+    public void CreatePath(Direction newDirection, Direction previousDirection)
+    {
+        Debug.LogFormat("Charting Course from {0} to {1}", previousDirection, newDirection);
+        if (newDirection == previousDirection)
+        {
+            throw new System.Exception("newDirection and previousDirection are the same");
+        }
+        
+        foreach(Direction direction in System.Enum.GetValues(typeof(Direction)))
+        {
+            if (direction != previousDirection && direction != newDirection)
+            {
+                CloseDoor(direction);
+            }
+            else
+            {
+                OpenDoor(direction);
+            }
+        }
+        
+        return;
+    }
 }
---
+++ Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour {
+
+    public GameObject BasicRoom;
+
+    public int NumberOfRooms;
+
+	// Use this for initialization
+	void Start () {
+        ////When done, use this to start
+        //Random.InitState(System.Environment.TickCount);
+        Random.InitState(1);
+
+        if (NumberOfRooms < 1)
+        {
+            Debug.Log("Number of rooms for Level Generation is Less then 1");
+        }
+
+        GenerateLevel(NumberOfRooms);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GenerateLevel(int numberOfRooms)
+    {
+        int roomsGenarated = 0;
+        GameObject previousRoom;
+        Direction previousDirection;
+        GameObject currentRoom;
+        Direction currentDirection;
+        Vector3 translationVector;
+        
+        // Initialize Level with first room
+        // Put room in scene
+        Debug.Log("Starting to generate rooms");
+        currentRoom = Instantiate(BasicRoom, transform);
+        
+        // determine direction
+        currentDirection = Direction.Right;
+
+        // Close extra doors
+        currentRoom.GetComponent<BasicRoom>().OpenDoor(currentDirection);
+
+        // set as previous room and direction
+        previousRoom = currentRoom;
+        previousDirection = currentDirection;
+        roomsGenarated++;
+
+        Debug.Log("First room generated, Moving to subsequent rooms");
+        while (roomsGenarated < numberOfRooms)
+        {
+            Debug.LogFormat("Generating room {0}", roomsGenarated);
+            currentRoom = Instantiate(GetNextRoom(), transform);
+            currentDirection = ChartNewCourse(previousDirection);
+
+            currentRoom.GetComponent<BasicRoom>().CreatePath(currentDirection, GetOppositeDirection(previousDirection));
+
+            //Position the room based on direction
+            switch(currentDirection)
+            {
+                case Direction.Top:
+                    translationVector = new Vector3(0, (currentRoom.GetComponent<BasicRoom>().Height / 2) + (previousRoom.GetComponent<BasicRoom>().Height / 2) + previousRoom.transform.position.y);
+                    break;
+                case Direction.Bottom:
+                    translationVector = new Vector3(0, 0);
+                    break;
+                case Direction.Left:
+                    translationVector = new Vector3(0, 0);
+                    break;
+                case Direction.Right:
+                default:
+                    translationVector = new Vector3((currentRoom.GetComponent<BasicRoom>().Width / 2) + (previousRoom.GetComponent<BasicRoom>().Width / 2) + previousRoom.transform.position.x, 0);
+                    break;
+            }
+            Debug.Log("Translating room");
+            Debug.Log(translationVector);
+            currentRoom.transform.Translate(translationVector);
+
+            // Set values for next iteration
+            previousDirection = currentDirection;
+            previousRoom = currentRoom;
+            roomsGenarated++;
+        }
+
+        return;
+    }
+
+    private GameObject GetNextRoom()
+    {
+        // Once you have more rooms, Make this random
+        return BasicRoom;
+    }
+
+    private Direction ChartNewCourse(Direction previousDirection)
+    {
+        // Make this a little more random
+
+        return Direction.Top;
+    }
+
+    private Direction GetOppositeDirection(Direction direction)
+    {
+        Direction returnValue;
+
+        switch(direction)
+        {
+            case Direction.Top:
+                returnValue = Direction.Bottom;
+                break;
+            case Direction.Bottom:
+                returnValue = Direction.Top;
+                break;
+            case Direction.Left:
+                returnValue = Direction.Right;
+                break;
+            case Direction.Right:
+            default:
+                returnValue = Direction.Left;
+                break;
+        }
+
+        Debug.LogFormat("GetOppositeDirection was given {0} and returned {1}", direction, returnValue);
+
+        return returnValue;
+    }
+}
+
+
+
+public enum Direction
+{
+    Top,
+    Bottom,
+    Left,
+    Right
+}
